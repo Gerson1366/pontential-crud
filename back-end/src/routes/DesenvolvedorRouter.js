455e5b32728f15d070781c61d2a195983117aa8d
Gerson <gerson@flybyte.com>
2021-10-04 17:03:29 -0300

    merge

--- back-end/src/routes/DesenvolvedorRouter.js
+++ back-end/src/routes/DesenvolvedorRouter.js
@@ -5,8 +5,10 @@ export default function (db) {
     const app = express();
     const controller = DesenvolvedorController(db);
     
-    app.get('/developers', controller.getDevelopers);
+    app.get('/developers/:id?', controller.getDevelopers);
     app.post('/developers', controller.postDeveloper);
+    app.put('/developers/:id',controller.putDeveloper);
+    app.delete('/developers/:id',controller.deleteDeveloper);
 
     return app
 }
